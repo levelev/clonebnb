3c0b4d9dfa07d7fa55fe448ccaef0e87c29591b5
Oleg Isakov <odisakov@gmail.com>
2017-11-21 11:24:56 +0100

    there was a typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
   resources :listings, except: [:destroy]
-  recources :bookings, only: [:show, :new, :create]
+  resources :bookings, only: [:show, :new, :create]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
