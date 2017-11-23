738ded050f0d439e26167f6a0ac72109793e9040
Alexander Pieczka <alexanderpieczka@ewe.net>
2017-11-23 11:10:06 +0100

    Price now returning less than and exact

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,6 @@ class PagesController < ApplicationController
     @listings = Listing.where(nil)
     @listings = @listings.where(city: params[:search][:city]) if params[:search][:city].present?
     @listings = @listings.where(category: params[:search][:category]) if params[:search][:category].present?
+    @listings = @listings.where("price <= ?", params[:search][:price]) if params[:search][:price].present?
   end
 end
