ff19bee3aab1a8bf104735ed2587517e4fbebc34
Oleg Isakov <odisakov@gmail.com>
2017-11-23 12:36:08 +0100

    parsing without photo for one category

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+require 'nokogiri'
+require 'open-uri'
 
 10.times do
   u = User.create!(
@@ -17,15 +19,32 @@ require 'faker'
     )
 end
 
+# Store URLs in variables
+
+@babysitter = Nokogiri::HTML(open('https://www.ebay-kleinanzeigen.de/s-babysitter-kinderbetreuung/berlin/anzeige:angebote/c290l3331'))
+@repairs = Nokogiri::HTML(open('https://www.ebay-kleinanzeigen.de/s-haus-garten/reparaturen/berlin/anzeige:angebote/c291l3331+dienstleistungen_haus_garten.art_s:reparaturen'))
+@cleaning = Nokogiri::HTML(open('https://www.ebay-kleinanzeigen.de/s-haus-garten/reingungsservice/berlin/anzeige:angebote/c291l3331+dienstleistungen_haus_garten.art_s:reingungsservice'))
+
+# Define enummeraters for parsing per category
+
+@identifier_babysitter = 1
+@identifier_repairs = 1
+@identifier_repairs = 1
+
+# Parsing per category and storage in string
 
 10.times do
+  @identifier_babysitter = 1 if @identifier_babysitter == 10
   r = Listing.create!(
-    title: Faker::SiliconValley.app,
-    price: Faker::Number.number(2),
-    description: Faker::SiliconValley.motto,
-    zip: %w(10103 11342 19343 13423 14035 10352).sample,
+    title: @babysitter.xpath("//*[@id='srchrslt-adtable']/li[#{@identifier_babysitter + 1}]/article/section[2]/h2").text.strip,
+    price: @babysitter.xpath("//*[@id='srchrslt-adtable']/li[#{@identifier_babysitter + 1}]/article/section[3]/strong").text.strip,
+    description: @babysitter.xpath("//*[@id='srchrslt-adtable']/li[#{@identifier_babysitter + 1}]/article/section[2]/p[1]/text()").text.strip,
+    zip: @babysitter.xpath("//*[@id='srchrslt-adtable']/li[#{@identifier_babysitter + 1}]/article/section[3]/text()[1]").text.strip.to_i,
     city: "Berlin",
-    category: %w(Handwerksleistungen Umzüge Reinigung).sample,
+    category: "Child Care",
     user_id: rand(1..10)
+#    photo: nil,
     )
+  @identifier_babysitter += 1
 end
+
